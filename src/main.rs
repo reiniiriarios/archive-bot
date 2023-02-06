8acfad063fc7d0b6af344d375ab7b44e7c52c946
Emma Litwa-Vulcu <emma@heysparkbox.com>
2023-02-06 14:32:12 -0800

    refactor: parse channel

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use substring::Substring;
 
 mod channels;
 mod slack_client;
@@ -7,26 +8,38 @@ mod types;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
   let api_key = env::var("SLACK_API_KEY").expect("Error: environment variable SLACK_API_KEY is not set.");
+  // TODO move to config
+  let prefixes = vec!["-"];
   if let Some(channels) = channels::get_channels(&api_key).await {
     for channel in channels {
-      if let (
-        Some(channel_id),
-        Some(name),
-        Some(is_member),
-      ) = (
-        channel.id,
-        channel.name,
-        channel.is_member,
-      ) {
-        println!("Channel: #{:}", name);
-        if is_member {
-          if let Some(history) = channels::get_history(&api_key, &channel_id, 1).await {
-            println!("  message: {:?}", history.first());
-          }
-        }
-      }
+      parse_channel(&api_key, channel, &prefixes).await;
     }
   }
 
   Ok(())
+}
+
+async fn parse_channel(api_key: &str, channel: types::Channel, ignore_prefixes: &Vec<&str>) {
+  if let (
+    Some(channel_id),
+    Some(name),
+    Some(is_member),
+  ) = (
+    channel.id,
+    channel.name,
+    channel.is_member,
+  ) {
+    println!("Channel: #{:}", name);
+    if ignore_prefixes.contains(&name.substring(0, 1)) {
+      println!("  prefix skipped");
+      return;
+    }
+
+    // TODO join channel
+    if is_member {
+      if let Some(history) = channels::get_history(&api_key, &channel_id, 1).await {
+        println!("  message: {:?}", history.first());
+      }
+    }
+  }
 }
